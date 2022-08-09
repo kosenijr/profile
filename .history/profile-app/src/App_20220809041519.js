2863c2f07a09dede149b5d43a8efe1d270ef627b
Kayode Oseni <52648319+kosenijr@users.noreply.github.com>
2022-08-09 05:00:06 -0400

    took care of coloration and spacing of navbar and elements within; might touch up more on it later

---
+++ .history/profile-app/src/App_20220809041519.js
@@ -0,0 +1,36 @@
+  // imports
+import './App.css';
+import './Navbar.css'
+// import Header from './components/Header'
+import Navbar from './components/Navbar'
+import Banner from './components/Banner'
+import About from './components/About'
+import Project from './components/Project'
+import Portfolio from './components/Portfolio'
+import Contact from './components/Contact'
+import Gallery from './components/Gallery'
+
+// App
+function App() {
+  return (
+    <div className="Background-Container">
+      <div className="App">
+        <Navbar />
+        <br />
+        <Banner />
+        <br />
+        <About />
+        <br />
+        <Project />
+        <br />
+        <Portfolio />
+        <br />
+        <Gallery />
+        <br />
+        <Contact />
+      </div>
+    </div>
+  );
+}
+
+export default App;
