97725edec784c416c822dcf1fca31bccb332ee03
Kayode Oseni <52648319+kosenijr@users.noreply.github.com>
2022-12-05 05:08:21 -0500

    capitalization of component files

--- .history/profile-app/src/components/Navbar_20221129003853.js
+++ .history/profile-app/src/components/Navbar_20221129003853.js
@@ -2,22 +2,26 @@
 import React from 'react';
 // must import pertinent css
 import '../Navbar.css'
-// import from react icons
-import {}
+import img from '../main-pictures/living-waters-1.png'
 
-
-// function
+// functions
 const Navbar = () => {
     return (
         // create main div with a class name for actual navbar
         <div className='Navbar'>
+            <div className='inner-nav'>
+                <img src={img} alt="logo" className='nav-spacing' id='living-water' />
+            </div>
             {/* span tag is an inline container used to mark up a part of a text or a part of a document: we will use for our logo */}
-            <span className='nav-logo'>MbK</span>
+            <div className='inner-nav' id='ndevr'>
+                <span className='nav-logo'><span className='nr'>n</span><span id='dev'>DEV</span><span className='nr'>r</span></span>
+            </div>
+
             {/* set up nav-items and 4 a tags: i.e., home, contact */}
-            <div className="nav-items">
-                <a href="/home">Home</a>
+            <div className="inner-nav nav-items">
+                <a href="/">Home</a>
                 <a href="/about">About</a>
-                <a href="/service">Service</a>
+                <a href="/portfolio">Portfolio</a>
                 <a href="/contact">Contact</a>
             </div>
         </div>
@@ -26,5 +30,5 @@ const Navbar = () => {
 }
 
 
-// export default
+// export defaultz
 export default Navbar
