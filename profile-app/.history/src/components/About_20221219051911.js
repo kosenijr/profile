b732cab190e22fe8dd3fe9a9798a4ba5da0f1000
Kayode Oseni <52648319+kosenijr@users.noreply.github.com>
2022-12-19 05:28:48 -0500

    made changes to navbar including the clickability of oseni to navigate to about me page; modified text content of about me page to include more relatable and concise wording of author's intent with site; made a colored-consistency with navbar; used text-align and width customization to improve viewing of about me section.

---
+++ profile-app/.history/src/components/About_20221219051911.js
@@ -0,0 +1,32 @@
+// imports
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { FaArrowCircleLeft, FaArrowCircleRight } from 'react-icons/fa';
+import '../styles/About.css';
+
+
+
+// function
+const About = () => {
+    const navigate = useNavigate();
+    return (
+        <div className='main-container' id='about-background'>
+            <div className='desc-sub-container' id='my-summary'>
+                <h1 className='title'>About Me</h1>
+                <p>My first name is pronounced kai·yuh·day. It means that I bring joy, so please don't let me delay!</p>
+                <p>This site consists of personal pictures taken by my wife and me before, during, and after the birth of our son.</p>
+                <br />
+                <p>As for my tech projects, those are located in the portfolio section. I pray that you'll join us on my humbling journey of photogrpahy programming.</p>
+                <br />
+                <p>Thank you for the support and I look forward to making a great impact!</p>
+            </div>
+            <div className='arrows-sub-container'>
+                <FaArrowCircleLeft className='arrows' onClick={() => navigate('/')} /><FaArrowCircleRight className='arrows' onClick={() => navigate('/portfolio')} />
+            </div>
+        </div >
+    )
+}
+
+
+// export
+export default About
