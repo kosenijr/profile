ecd8aaca0e7de45b49a380cb4877e2aa63a1a38a
Kayode Oseni <52648319+kosenijr@users.noreply.github.com>
2022-12-24 07:07:31 -0500

    transitioned useState hook to Form.js from Contact.js

--- profile-app/.history/src/components/Contact_20221224070601.js
+++ profile-app/.history/src/components/Contact_20221224070601.js
@@ -1,5 +1,5 @@
 // imports
-import React from 'react';
+import React, { useState } from 'react';
 import { FaArrowCircleLeft, FaArrowCircleRight } from 'react-icons/fa';
 import { useNavigate } from 'react-router-dom'
 import Form from './Form'
--- profile-app/.history/src/components/Form_20221224070614.js
+++ profile-app/.history/src/components/Form_20221224070614.js
@@ -1,4 +1,3 @@
-import React, { useState } from 'react';
 import '../styles/Form.css'
 
 const Form = () => {
