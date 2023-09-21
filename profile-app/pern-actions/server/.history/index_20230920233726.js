4d873ebf2a7485144ed2ddbe16e0e5ba968feee3
Kayode Oseni <k.oseni.jr@gmail.com>
2023-09-20 23:38:19 -0400

    setting up server and database

---
+++ profile-app/pern-actions/server/.history/index_20230920233726.js
@@ -0,0 +1,13 @@
+// express creates the server
+// pg connects database to server
+// cors allows for two domains (or more) to talk to each other
+
+//require the express package and store the function within the app variable
+const express = require("express");
+const [app, port] = [express(), 5000];
+
+// set express to listen at Port 500
+
+app.listen(port, () => {
+    console.log(`server has started on port ${port}`);
+})
